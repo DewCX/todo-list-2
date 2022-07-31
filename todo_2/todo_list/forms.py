fc037c1f240e025a122f93b7a5c2084ecd2e431b
Cem Mandraci <cemmandraci@hotmail.com>
2022-07-31 22:57:59 +0300

    #created forms.py
    # Activate csrf token
    # Updated index.html

---
+++ todo_2/todo_list/forms.py
@@ -0,0 +1,7 @@
+from tkinter import Widget
+from django import forms
+
+class TodoForm(forms.Form):
+    text = forms.CharField(max_length=50,
+        widget = forms.TextInput(
+            attrs={'class':'form-control', 'placeholder': 'Enter todo e.g. Delete junk files', 'aria-label' : 'Todo', 'aria-describedby': 'add-btn'}))
--- todo_2/todo_list/views.py
+++ todo_2/todo_list/views.py
@@ -1,10 +1,15 @@
+import imp
 from django.shortcuts import render
 from .models import Todo
-
+from .forms import TodoForm
 
 def index(request):
     todo_list = Todo.objects.order_by('id')
+
+    form = TodoForm()
+
     context = {
         'todo_list': todo_list,
+        'form': form
     }
     return render(request, 'todo/index.html', context)
